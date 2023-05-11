a718d14d51f56ad287c95802873764acfb0e4ff3
wlgns1501 <wlgns1501@gmail.com>
2023-05-11 14:43:26 +0900

    lesson apply

--- src/lesson/lesson.service.ts
+++ src/lesson/lesson.service.ts
@@ -14,11 +14,6 @@ import { Cron, CronExpression } from '@nestjs/schedule';
 import { UserRepository } from 'src/repositories/user.repository';
 import { Lesson } from 'src/entities/lesson.entity';
 
-type APPLY_USER = {
-  userId: number;
-  lessonId: number;
-};
-
 export const TYPE_LESSON_STATUS = {
   ACTIVE: 'ACTIVE',
   CLOSED: 'CLOSED',
@@ -31,38 +26,11 @@ export const TYPE_UPDATE_PARTICIPANT = {
 
 @Injectable()
 export class LessonService {
-  private applyQueue: Array<{ userId: number; lessonId: number }> = [
-    // { userId: 1, lessonId: 2 },
-    // { userId: 2, lessonId: 10 },
-    // { userId: 1, lessonId: 11 },
-    // { userId: 1, lessonId: 12 },
-    // { userId: 1, lessonId: 22 },
-    // { userId: 1, lessonId: 32 },
-    // { userId: 1, lessonId: 42 },
-    // { userId: 1, lessonId: 52 },
-    // { userId: 1, lessonId: 62 },
-    // { userId: 1, lessonId: 72 },
-    // { userId: 1, lessonId: 82 },
-  ];
-  private isQueueLocked = false;
   private lessonRepository: LessonRepository;
   private subCategoryRepository: SubCategoryRepository;
   private userLessonRepository: UserLessonRepository;
   private userRepository: UserRepository;
   constructor(private readonly connection: Connection) {}
-
-  private calculateRetryAfter(): number {
-    const now = Date.now();
-    const nextUnlockTime = this.getNextUnlockTime();
-    const retryAfter = Math.ceil((nextUnlockTime - now) / 1000);
-    return retryAfter;
-  }
-
-  private getNextUnlockTime(): number {
-    const now = Date.now();
-    const nextUnlockTime = now + 5000;
-    return nextUnlockTime;
-  }
 
   private async checkUserLimit(lessonId: number) {
     const lesson = await this.getLesson(lessonId);
@@ -229,97 +197,27 @@ export class LessonService {
       this.connection.getCustomRepository(UserLessonRepository);
     this.userRepository = this.connection.getCustomRepository(UserRepository);
 
-    // 대기열이 꽉차서 대기열 lock 된 경우
-    if (this.isQueueLocked) {
-      const retryAfter = this.calculateRetryAfter();
-      throw new HttpException(
-        {
-          message: HTTP_ERROR.APPLY_QUEUE_IS_FULL,
-          detail: `대기열이 꽉 찬 상태입니다. ${retryAfter}후에 다시 시도해 주세요`,
-        },
-        HttpStatus.BAD_REQUEST,
-      );
-    }
-
-    // 똑같은 유저가 똑같은 레슨에 여러번 수강하는 경우
-    const alreadyInQueue = this.applyQueue.some(
-      (item) => item.userId === user.id && item.lessonId === lessonId,
-    );
-
-    if (alreadyInQueue) {
-      throw new HttpException(
-        {
-          message: HTTP_ERROR.ALREADY_IN_QUEUE,
-          detail: '이미 대기열에 있습니다.',
-        },
-        HttpStatus.BAD_REQUEST,
-      );
-    }
-
     const lesson = await this.checkUserLimit(lessonId);
 
     await this.checkAlreadyApply(user, lesson);
 
-    if (this.applyQueue.length >= 100) this.isQueueLocked = true;
+    await this.userLessonRepository.applyLesson(user, lesson);
 
-    let applyUser: APPLY_USER;
+    const { raw } = await this.lessonRepository.updateParticipantCount(
+      lessonId,
+      TYPE_UPDATE_PARTICIPANT.PLUS,
+    );
 
-    if (this.applyQueue.length === 0) {
-      applyUser = { userId: user.id, lessonId };
-    } else if (this.applyQueue.length > 0) {
-      applyUser = this.applyQueue.shift();
-    }
+    const [checkCount] = raw;
 
-    if (this.applyQueue.length <= 10) {
-      const { userId, lessonId } = applyUser;
-
-      try {
-        const user = await this.userRepository.findOne({ id: userId });
-
-        await this.userLessonRepository.applyLesson(user, lesson);
-
-        const { raw } = await this.lessonRepository.updateParticipantCount(
-          lessonId,
-          TYPE_UPDATE_PARTICIPANT.PLUS,
-        );
-
-        const [checkCount] = raw;
-
-        if (checkCount.participantCount === 20) {
-          await this.lessonRepository.updateStatusLesson(
-            lessonId,
-            TYPE_LESSON_STATUS.CLOSED,
-          );
-        }
-        console.log(this.applyQueue);
-
-        return { success: true };
-      } catch (error) {
-        // this.applyQueue.push({ userId, lessonId });
-
-        throw new HttpException(
-          {
-            message: HTTP_ERROR.BAD_REQUEST,
-            detail: '해당 레슨은 수강 인원이 꽉 찼습니다.',
-          },
-          HttpStatus.BAD_REQUEST,
-        );
-      }
-    } else {
-      this.applyQueue.push({ userId: user.id, lessonId });
-
-      if (this.applyQueue.length === 100) {
-        this.isQueueLocked = true;
-      }
-
-      throw new HttpException(
-        {
-          message: HTTP_ERROR.BAD_REQUEST,
-          detail: '대기열에 추가 되었습니다.',
-        },
-        HttpStatus.BAD_REQUEST,
+    if (checkCount.participantCount === 20) {
+      await this.lessonRepository.updateStatusLesson(
+        lessonId,
+        TYPE_LESSON_STATUS.CLOSED,
       );
     }
+
+    return { success: true };
   }
 
   @Transactional()
@@ -360,14 +258,4 @@ export class LessonService {
 
     return { success: true };
   }
-
-  // @Cron(CronExpression.EVERY_30_SECONDS)
-  // async applyInQueue() {
-  //   this.lessonRepository =
-  //     this.connection.getCustomRepository(LessonRepository);
-  //   this.userLessonRepository =
-  //     this.connection.getCustomRepository(UserLessonRepository);
-  //   this.userRepository = this.connection.getCustomRepository(UserRepository);
-
-  // }
 }
